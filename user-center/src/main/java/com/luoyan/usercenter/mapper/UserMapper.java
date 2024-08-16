24075e4596640a3a6099a3085889d66ab4f94c98
只会CV <528375822@qq.com>
2024-08-16 21:42:27 +0800

    笔记更改

--- user-center/src/main/java/com/luoyan/usercenter/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.luoyan.usercenter.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.luoyan.usercenter.model.User;
-
-public interface UserMapper extends BaseMapper<User> {
-
-}
